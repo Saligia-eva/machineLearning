9e153f6a8fc295f9f722d35eaaa444a8741be919
saligia <782112163@qq.com>
2017-11-14 21:48:49 +0800

    modify

---
+++ 3-Bayes/bayes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/evn python
+# -*- coding:utf8 -*-
+
+################################
+# 构建单词向量:
+#      0 : 情感类评论
+#      1 : 中性言论
+#################################
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
+    return postingList,classVec
+
+
+def createVocabList(dataSet):
+    """
+    获取　dataset 下的所有元素集合
+    """
+    vocabSet = set([])  #create empty set
+    for document in dataSet:
+        vocabSet = vocabSet | set(document) #union of the two sets
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    # 构建 0 向量矩阵
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else: print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+def main():
+    database,labels=loadDataSet()
+
+    myVocabList=createVocabList(database)
+
+    res = setOfWords2Vec(myVocabList, database[0])
+
+    print(res)
+
+if __name__ == '__main__':
+    main()
