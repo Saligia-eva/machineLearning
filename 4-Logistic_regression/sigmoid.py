29e2675d013968c5891bc24ca129aeebe695537d
saligia <782112163@qq.com>
2017-11-17 20:02:10 +0800

    add

--- 4-Logistic_regression/sigmoid.py
+++ 4-Logistic_regression/sigmoid.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# sigmoid 函数
 def sigmoid():
     x = np.linspace(-8,8,1000)
     y = [1/(1+np.exp(-i)) for i in x]
@@ -21,8 +22,27 @@ def sigmoid():
     plt.show()
 
 
+# 三维绘图
+def  gradientFunc():
+    from mpl_toolkits.mplot3d import Axes3D
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    X = np.arange(-2, 2, 0.1)
+    Y = np.arange(-2, 2, 0.1)
+    X, Y = np.meshgrid(X, Y)
+    Z = X**2 + Y**2
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow')
+
+    x= np.linspace(1,2,10)
+    y= np.linspace(2,4,10)
+
+    plt.plot(x,y)
+    
+    plt.show()
+
 def main():
-    sigmoid()
+    gradientFunc()
 
 if __name__ == '__main__':
     main()
